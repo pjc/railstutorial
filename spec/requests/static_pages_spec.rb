7a1adcdc35e4c439a31fb8573476668e6aa655c8
Peter-Jan Celis <pj@celis.org>
2012-11-20 18:34:20 +0100

    Refactor rspec requests (Exercises chapter 5).

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,36 +5,56 @@ describe "Static Pages" do
   # let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector 'h1',     text: heading }
+    it { should have_selector 'title',  text: full_title(page_title) }
+  end
+
   describe "Home page" do
+    before            { visit root_path }
+    let(:heading)     { 'Sample App' }
+    let(:page_title)  { ''}
 
-    before { visit root_path }
-
-    it { should have_selector('h1',         text: 'Sample App') }
-    it { should have_selector('title',      text: full_title('')) }
-    it { should_not have_selector('title',  text: "| Home") }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title',  text: "| Home" }
   end
 
   describe "Help page" do
+    before           { visit help_path }
+  	let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    before { visit help_path }
-  	
-    it { should have_selector('h1',     text: 'Help') }
-    it { should have_selector('title',  text: full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
+    before            { visit about_path }
+    let(:heading)     { 'About Us' }
+    let(:page_title)  { 'About' }
 
-    before { visit about_path }
-
-    it { should have_selector('h1',     text: 'About Us') }    
-    it { should have_selector('title',  text: full_title('About')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
+    before            { visit contact_path }
+    let(:heading)     { 'Contact' }
+    let(:page_title)  { 'Contact'}
 
-    before { visit contact_path }
+    it_should_behave_like "all static pages"
+  end
 
-    it { should have_selector('h1',     text: 'Contact') }
-    it { should have_selector('title',  text: full_title('Contact')) }
+  it "should have the right links on the layout" do
+    visit root_path
+    
+    %w(About Help Contact).each do |page_name|
+      click_link "#{page_name}"
+      page.should have_selector 'title', text: full_title("#{page_name}")
+    end
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
   end
 end
